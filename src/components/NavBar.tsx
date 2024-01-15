b663024a0f9353c18439ea96a5d6406254db8cde
Rohit Mokashi <97401042+rohitmokashi@users.noreply.github.com>
2024-01-15 20:50:36 +0530

    added portfolio pdf

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,29 @@
-import './NavBar.css';
-import { BsFillMoonStarsFill } from 'react-icons/bs';
+import "./NavBar.css";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
-function NavBar () {
-    return (
-        <nav className='nav-bar'>
-            <h1 className='nav-bar-h1'>Portfolio</h1>
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <h1 className="nav-bar-h1">Portfolio</h1>
 
-            <ul className='nav-bar-ul'>
-                <li className='nav-bar-li'><BsFillMoonStarsFill /></li>
-                <li className='nav-bar-li resume-li'><a href="#">Resume</a></li>
-            </ul>
-        </nav>
-    )
+      <ul className="nav-bar-ul">
+        <li className="nav-bar-li">
+          <BsFillMoonStarsFill />
+        </li>
+        <li className="nav-bar-li resume-li">
+          <a
+            href="../software_development_resume.pdf"
+            target="_blank"
+            type="application/pdf"
+            rel="alternate"
+            media="print"
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
